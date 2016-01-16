980bd2dbc24513b82249803a482edeb1126caa56
= <me@matheuslc.com>
2016-01-16 18:51:01 -0200

    News components CSS rules; Add these rules to React componenets; Bug fix in the semantic grid file

--- src/js/UI/News/author.jsx
+++ src/js/UI/News/author.jsx
@@ -4,7 +4,7 @@ export default class Author extends React.Component {
     render() {
       return <div className='author' rel='author'>
         <img src={this.props.image} alt={this.props.name} className='author-img'/>
-        <span className='author-name'>by {this.props.name}</span>
+        <span className='author-name author-name-primary'>by {this.props.name}</span>
       </div>
     }
 }
--- src/js/UI/News/singleNews.jsx
+++ src/js/UI/News/singleNews.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { Image } from 'react-picture';
 import Author from './author.jsx';
+import classNames from 'classnames';
 
 export default class SingleNews extends React.Component {
   render() {
-    return <article className="news-content">
-      <span>{this.props.category}</span>
+    let categoryClass = 'news-category ';
+        categoryClass += 'category-' + this.props.category.toLowerCase();
 
-      <header>
-        <h1>{this.props.title}</h1>
+    return <article className='news-content'>
+      <span className={categoryClass}>{this.props.category}</span>
+
+      <div className='news-img'>
+        <Image alt='Your picture description' srcSet={require('url?mimetype=image/png!../../../img/news_01@2x.png')} extra={{className:'myImg'}}/>
+      </div>
+
+      <header className='news-header'>
+        <h1 className='news-title news-title-primary'>{this.props.title}</h1>
       </header>
 
       <Author name={this.props.authorName} image={this.props.authorImg}></Author>
 
-      <div>
+      <div className='news-text news-text-primary'>
         <p>{this.props.text}</p>
       </div>
     </article>
